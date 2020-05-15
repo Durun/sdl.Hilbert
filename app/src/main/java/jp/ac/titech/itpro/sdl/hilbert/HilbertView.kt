62c5a155467b6d23078b7c8dec46cebf9b498b40
Durun <negiigainuki@gmail.com>
2020-05-15 23:06:38 +0900

    draw asynchronously

--- app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/HilbertView.kt
+++ app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/HilbertView.kt
@@ -1,6 +1,7 @@
 package jp.ac.titech.itpro.sdl.hilbert
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
@@ -11,28 +12,41 @@ import java.lang.Math.min
 
 class HilbertView @JvmOverloads constructor(context: Context?, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : View(context, attrs, defStyleAttr) {
     private val paint = Paint()
-    private lateinit var canvas: Canvas
+    private lateinit var bufferBitmap: Bitmap
+    private lateinit var bufferCanvas: Canvas
     private var order = 1
-    private val turtle = HilbertTurtle(Drawer { x0, y0, x1, y1 -> canvas.drawLine(x0.toFloat(), y0.toFloat(), x1.toFloat(), y1.toFloat(), paint) })
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        bufferBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        bufferCanvas = Canvas(bufferBitmap)
+    }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        this.canvas = canvas
-        val w = canvas.width
-        val h = canvas.height
-        paint.color = Color.DKGRAY
-        canvas.drawRect(0f, 0f, w.toFloat(), h.toFloat(), paint)
-        paint.color = Color.WHITE
-        paint.strokeWidth = 3f
-        val size = min(w, h)
-        val step = size.toDouble() / (1 shl order)
-        turtle.setPos((w - size + step) / 2, (h + size - step) / 2)
-        turtle.setDir(HilbertTurtle.E)
-        turtle.draw(order, step, HilbertTurtle.R)
+        canvas.drawBitmap(bufferBitmap, 0f, 0f, paint)
     }
 
     fun setOrder(n: Int) {
         order = n
-        invalidate()
+        post {
+            bufferCanvas.drawHilbert()
+            invalidate()
+        }
+    }
+
+    private fun Canvas.drawHilbert() {
+        val w = width
+        val h = height
+        paint.color = Color.DKGRAY
+        this.drawRect(0f, 0f, w.toFloat(), h.toFloat(), paint)
+        paint.color = Color.WHITE
+        paint.strokeWidth = 3f
+        val size = min(w, h)
+        val step = size.toDouble() / (1 shl order)
+        val turtle = HilbertTurtle(Drawer { x0, y0, x1, y1 -> this.drawLine(x0.toFloat(), y0.toFloat(), x1.toFloat(), y1.toFloat(), paint) })
+        turtle.setPos((w - size + step) / 2, (h + size - step) / 2)
+        turtle.setDir(HilbertTurtle.E)
+        turtle.draw(order, step, HilbertTurtle.R)
     }
 }
